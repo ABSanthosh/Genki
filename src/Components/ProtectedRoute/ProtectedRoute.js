fd1a46dc3d6d1a4489930d3e8dc725d14c8b5109
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-16 17:59:41 +0530

    feat(Components): Protected route components added

---
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import { useAuth } from "../../hooks/useAuth";
+import Delay from "react-delay";
+import { Redirect, Route } from "react-router-dom";
+
+export default function ProtectedRoute(props) {
+  const { userState, status } = useAuth();
+
+  if (status === "ready" && userState) {
+    return <Route {...props} />;
+  }
+  if (userState !== null) {
+    return <Delay wait={250}>Loading...</Delay>;
+  }
+
+  return <Redirect to="/" />;
+}
