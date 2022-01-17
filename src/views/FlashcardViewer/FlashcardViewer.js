e3a2236502822425bf8b8a2cb885c99592f2bbfa
ABSanthosh <a.b.santhosh02@gmail.com>
2022-01-18 02:00:57 +0530

    feat(Page): Added flashcard viewer route

--- src/views/FlashcardViewer/FlashcardViewer.js
+++ src/views/FlashcardViewer/FlashcardViewer.js
@@ -1,26 +1,40 @@
 import React from "react";
 // import PropTypes from "prop-types";
-import { useStoreState } from "easy-peasy";
+import { useStoreRehydrated, useStoreState } from "easy-peasy";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import "./FlashcardViewer.scss";
+import FlipCard from "../../Components/FlipCard/FlipCard";
+import CardScroller from "../../Components/CardScroller/CardScroller";
 
 function FlashcardViewer(props) {
   const { deckId } = useParams();
   const currentDeck = useStoreState((state) => state.currentDeck);
+  const isRehydrated = useStoreRehydrated();
+
+  console.log(isRehydrated, currentDeck);
+  const childArray = isRehydrated
+    ? currentDeck.cards.map((item, index) => (
+        <FlipCard key={item.id} front={item.front} back={item.back} />
+      ))
+    : [];
 
   return (
-    <div className="FlashcardViewerWrapper">
-      {/* <p>{deckId}</p> */}
-      {/* <pre>{JSON.stringify(currentDeck, null, 2)}</pre> */}
-      <header className="FlashcardViewerWrapper__header">
-        <p>{currentDeck.title}</p>
-      </header>
-      <div className="FlashcardViewerWrapper__container">
-        <div className="FlashcardViewerWrapper__item">
-          
+    <>
+      {isRehydrated ? (
+        <div className="FlashcardViewerWrapper">
+          {/* <p>{deckId}</p> */}
+          {/* <pre>{JSON.stringify(currentDeck, null, 2)}</pre> */}
+          <header className="FlashcardViewerWrapper__header">
+            <p>{currentDeck.title}</p>
+          </header>
+          <div className="FlashcardViewerWrapper__container">
+            <CardScroller childArray={childArray} />
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
   );
 }
 
